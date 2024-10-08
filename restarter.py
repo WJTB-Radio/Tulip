b9c707895cf419a1d3fc2e35cace510d53c9a934
julia <alara@alarabread.fun>
2024-10-08 12:29:54 -0400

    do access control for restarter

--- restarter.py
+++ restarter.py
@@ -5,6 +5,8 @@ import util
 def add_commands(tree):
 	@tree.command(name="restartstream", description="Run this command to restart the stream if butt wont connect.", guild=discord.Object(id=util.GUILD_ID))
 	async def restartstream(context):
+		if(not await util.check_admin(context)):
+			return
 		subprocess_result = subprocess.run(["/home/admin/tulip/restart_stream.sh"], capture_output=True)
 		result = subprocess_result.stdout.decode("utf-8").strip()
 		message = "The stream has been restarted. BUTT should reconnect." if result == "ok" else "Something went wrong, ask your local web person for help."
