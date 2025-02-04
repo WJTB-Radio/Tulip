531ef75922c17c1a59551f54a4b464a4cb8bdf44
julia <59781761+AlaraBread@users.noreply.github.com>
2025-02-04 11:39:21 -0500

    make resize.sh position independent

--- resize.sh
+++ resize.sh
@@ -1,44 +1,40 @@
 #!/bin/bash
 
-cd /home/admin/show_data
+cd ../show_data
 
-if [ -z "$1" ]
-then
-	>&2 echo "error: invalid parameters"
+if [ -z "$1" ]; then
+	echo >&2 "error: invalid parameters"
 	exit
 fi
 
-filename=`date +%s%N`
+filename=$(date +%s%N)
 
 curl -s $1 -o "${filename}_r.jpg"
 
-if [ $? != 0 ]
-then
-	>&2 echo "error: failed to download image"
-	rm /home/admin/show_data/*.jpg
+if [ $? != 0 ]; then
+	echo >&2 "error: failed to download image"
+	rm *.jpg
 	exit
 fi
 
 convert "${filename}_r.jpg" -resize 750000@ "${filename}_c.jpg"
 
-if [ $? != 0 ]
-then
-	>&2 echo "error: failed to convert image"
-	rm /home/admin/show_data/*.jpg
+if [ $? != 0 ]; then
+	echo >&2 "error: failed to convert image"
+	rm *.jpg
 	exit
 fi
 
 rm "${filename}_r.jpg"
 
-hashed=`md5sum "${filename}_c.jpg" | cut -f1 "-d "`
+hashed=$(md5sum "${filename}_c.jpg" | cut -f1 "-d ")
 
-if [ ! -d images ]
-then
+if [ ! -d images ]; then
 	mkdir images
 fi
 
 mv "${filename}_c.jpg" ./images/${hashed}.jpg
 
-rm /home/admin/show_data/*.jpg
+rm *.jpg
 
 echo ${hashed}.jpg
